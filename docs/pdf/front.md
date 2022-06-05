807d624cd5a574ac810491fae21b0c892767787d
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2022-06-05 13:23:17 +0300

    Pdf front page

---
+++ docs/pdf/front.md
@@ -0,0 +1,25 @@
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<center>
+
+<div class="book-title"> Zhaba-script documentation </div>
+Владимир Мацюк
+<br/>
+2022
+
+</center>
+
+<div style="page-break-after: always;"></div>
+
+[TOC]
--- docs/pdf/make.py
+++ docs/pdf/make.py
@@ -2,14 +2,17 @@ from markdown import markdown
 from os import path
 from glob import glob
 from weasyprint import HTML
+from markdown.extensions.toc import TocExtension
 
 dir = path.dirname(path.realpath(__file__))
 
-filenames = sorted(glob(dir + '/../*.md'),
+filenames = [dir + '/front.md']
+filenames += sorted(glob(dir + '/../*.md'),
                    key=lambda s: (int(s.split('\\')[-1].split(' ')[0])))
-
-md = "".join(["".join(open(fname, encoding='utf-8')) +
-             '<div style="page-break-after: always;"></div>' for fname in filenames])
+str = "".join(["".join(open(fname, encoding='utf-8')) +
+               '<div style="page-break-after: always;"></div>' for fname in filenames])
+md = markdown(str, extensions=['fenced_code',
+              'tables', TocExtension(title='Contents')])
 
 html = f"""<!DOCTYPE html>
   <html>
@@ -19,11 +22,11 @@ html = f"""<!DOCTYPE html>
     </head>
   <body>
     <div class="markdown-body">
-      {markdown(md, extensions=['fenced_code', 'tables'])}
+      {md}
     </div>
   </body>
 </html>
 """
 
-# open(dir + '/merged.html', 'w', encoding='utf-8').write(html)
+open(dir + '/merged.html', 'w', encoding='utf-8').write(html)
 open(dir + '/merged.pdf', 'wb').write(HTML(string=html).write_pdf())
