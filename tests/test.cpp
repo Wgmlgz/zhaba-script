a7c08fd7304d967c5bd88de4d781fb0ebfac9b02
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2022-06-23 19:34:45 +0300

    Tests

---
+++ tests/test.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+#include "zhaba/zhaba.hpp"
+
+using namespace std::string_literals;
+
+std::string run(std::string file_name) {
+  int argc = 2;
+  char* tmp[2] = {(char*)"test", (char*)file_name.c_str()};
+  char** argv = tmp;
+
+  testing::internal::CaptureStdout();
+  zh_main(argc, argv);
+  std::string output = testing::internal::GetCapturedStdout();
+  return output;
+}
+
+std::string runB(std::string file_name) {
+  int argc = 3;
+  char* tmp[3] = {(char*)"test", (char*)file_name.c_str(), (char*)"--B"};
+  char** argv = tmp;
+
+  testing::internal::CaptureStdout();
+  zh_main(argc, argv);
+  std::string output = testing::internal::GetCapturedStdout();
+  return output;
+}
+
+TEST(Bytecode, Brainfuck) {
+  EXPECT_EQ("Hello World!\n", run("../examples/brainfuck.zh"));
+}
+
+TEST(GCC, Brainfuck) {
+  EXPECT_EQ("Hello World!\n", runB("../examples/brainfuck.zh"));
+}
